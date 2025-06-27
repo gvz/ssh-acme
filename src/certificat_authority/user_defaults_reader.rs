d11da17a5a1f4e002dc0a0f41fd96fdcd7c302e3
Georg von Zengen <github@vz-ge.org>
2025-06-27 11:34:11 +0200

    more config management

--- src/certificat_authority/user_defaults_reader.rs
+++ src/certificat_authority/user_defaults_reader.rs
@@ -1,40 +1,29 @@
 use std::fs::File;
 use std::io::Read;
+use std::time::Duration;
 use std::{collections::HashMap, hash::Hash};
 
 use anyhow::Result;
+use duration_str::parse as parse_duration;
 use minijinja::{Environment, context};
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use toml;
 
 #[derive(Deserialize, Debug)]
 pub struct UserDefaults {
-    pub validity: String,
+    pub validity: Duration,
     pub principals: Vec<String>,
     pub extensions: Vec<String>,
     pub critical_options: HashMap<String, String>,
 }
 
-pub fn read_user_defaults(file_path: &str) -> Result<CertificateConfig> {
+pub fn read_user_defaults(file_path: &str) -> Result<UserDefaults> {
     let mut config_file = File::open(file_path)?;
-    let mut config = String::new();
-    let _ = config_file.read_to_string(&mut config)?;
+    let mut config_text = String::new();
+    let _ = config_file.read_to_string(&mut config_text)?;
 
-    let mut env = Environment::new();
-    env.add_template("config", &config).unwrap();
-    let tmpl = env.get_template("config").unwrap();
-    let mut critical_options: HashMap<String, String> = HashMap::new();
-    critical_options.insert("test".to_string(), "map".to_string());
-    let json = tmpl
-        .render(context!(
-        KeyID => 200,
-        Principals => vec!("test", "list"),
-        CriticalOptions => critical_options,
-        ValiditySpan => 24*30
-        ))
-        .unwrap();
+    let config: UserDefaults = toml::from_str(&config_text)?;
 
-    let config: CertificateConfig = toml::from_str(&json)?;
     Ok(config)
 }
 
@@ -42,8 +31,8 @@ pub fn read_user_defaults(file_path: &str) -> Result<CertificateConfig> {
 mod test {
     use super::*;
     #[test]
-    fn read_cert_config_test() {
-        let config = read_certificate_config("./test_data/user_cert_config.toml").unwrap();
-        println!("config: {:?}", config);
+    fn read_user_defaults_test() {
+        let config = read_user_defaults("./config/user_default.toml").unwrap();
+        println!("{:?}", config.validity);
     }
 }
