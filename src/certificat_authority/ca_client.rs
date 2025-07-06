ce12b3fccfba3b3ac95bb6a56f12b1c8df2a924f
Georg von Zengen <oni303@gmail.com>
2025-07-06 13:34:04 +0200

    build integration test

--- src/certificat_authority/ca_client.rs
+++ src/certificat_authority/ca_client.rs
@@ -1,6 +1,7 @@
-use std::os::unix::net::UnixStream;
-use std::io::{Read, Write};
 use anyhow::Result;
+use log::{debug, info};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::UnixStream;
 
 use super::{CaRequest, CaResponse};
 
@@ -14,14 +15,16 @@ impl CaClient {
         CaClient { socket_path }
     }
 
-    pub fn send_request(&self, request: &CaRequest) -> Result<CaResponse> {
-        let mut stream = UnixStream::connect(&self.socket_path)?;
+    pub async fn send_request(&self, request: &CaRequest) -> Result<CaResponse> {
+        debug!("connection to: {}", self.socket_path);
+        let mut stream = UnixStream::connect(&self.socket_path).await?;
         let request_json = serde_json::to_string(request)?;
-        stream.write_all(request_json.as_bytes())?;
-        stream.shutdown(std::net::Shutdown::Write)?; // Signal end of request
+        stream.write_all(request_json.as_bytes()).await?;
+        debug!("wrote to: {}", self.socket_path);
 
         let mut response_json = String::new();
-        stream.read_to_string(&mut response_json)?;
+        stream.read_to_string(&mut response_json).await?;
+        debug!("red from: {}, {}", self.socket_path, response_json);
         let response: CaResponse = serde_json::from_str(&response_json)?;
         Ok(response)
     }
--- src/certificat_authority/mod.rs
+++ src/certificat_authority/mod.rs
@@ -3,6 +3,7 @@ use std::io::Read;
 use std::path::PathBuf;
 
 use anyhow::Result;
+use log::info;
 use serde::{Deserialize, Serialize};
 use ssh_key::rand_core::OsRng;
 use ssh_key::{
@@ -66,8 +67,8 @@ pub enum CaRequest {
     SignCertificate {
         public_key: PublicKey,
         principals: Vec<String>,
-        valid_before: u64,
         valid_after: u64,
+        valid_before: u64,
     },
 }
 
